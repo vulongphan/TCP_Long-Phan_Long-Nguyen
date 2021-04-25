033cb8c0c6783b90d32b0b7337825b45349252f1
Long Phan <pvlong2000@gmail.com>
2021-04-25 15:57:04 +0400

    refactor code for receiver

--- rdt_receiver.c
+++ rdt_receiver.c
@@ -12,28 +12,47 @@
 #include "common.h"
 #include "packet.h"
 
-/*
- * You ar required to change the implementation to support
- * window size greater than one.
- * In the currenlt implemenetation window size is one, hence we have
- * onlyt one send and receive packet
- */
-tcp_packet *recvpkt;
-tcp_packet *sndpkt;
+int sockfd;                    /* socket */
+int portno;                    /* port to listen on */
+int clientlen;                 /* byte size of client's address */
+struct sockaddr_in serveraddr; /* server's addr */
+struct sockaddr_in clientaddr; /* client addr */
+FILE *fp;
+
+
+void write_to_file(char* file_name, FILE* fp, int pos, char* data, int len);
+void send_ACK(int ackno);
+
+void write_to_file(char* file_name, FILE* fp, int pos, char* data, int len) {
+    fp = fopen(file_name, "r+");
+    fseek(fp, pos, SEEK_SET);
+    fwrite(data, 1, len, fp);
+    fclose(fp);
+    printf("pkt with sequence number %d, data_size %d, written to file\n", pos, len);
+
+}
+
+void send_ACK(int ackno) {
+    tcp_packet *sndpkt = make_packet(0);
+    sndpkt->hdr.ackno = ackno; 
+    sndpkt->hdr.ctr_flags = ACK;
+    if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0,
+               (struct sockaddr *)&clientaddr, clientlen) < 0)
+    {
+        error("ERROR in sendto");
+    }
+
+}
 
 int main(int argc, char **argv)
 {
-    int sockfd;                    /* socket */
-    int portno;                    /* port to listen on */
-    int clientlen;                 /* byte size of client's address */
-    struct sockaddr_in serveraddr; /* server's addr */
-    struct sockaddr_in clientaddr; /* client addr */
     int optval;                    /* flag value for setsockopt */
-    FILE *fp;
+    tcp_packet *recvpkt;
+    char* file_name;
     char buffer[MSS_SIZE];
     struct timeval tp;
     int cur_seqno;
-    int next_seqno;
+    int ackno;
 
     /* 
      * check command line arguments 
@@ -44,8 +63,9 @@ int main(int argc, char **argv)
         exit(1);
     }
     portno = atoi(argv[1]);
+    file_name = argv[2];
 
-    fp = fopen(argv[2], "w");
+    fp = fopen(file_name, "w");
     if (fp == NULL)
     {
         error(argv[2]);
@@ -89,12 +109,15 @@ int main(int argc, char **argv)
 
     clientlen = sizeof(clientaddr);
     cur_seqno = 0; // sequence number of the previous pkt
+
+    // need a buffer size = N * DATA_SIZE to store out-of-order pkts
+    // note that these out-of-order pkts are in increasing sequence
+    // upon receiving an expected pkt (recvpkt->hdr.seqno + recvpkt->hdr.data_size == exp_sepno)
     while (1)
     {
         /*
          * recvfrom: receive a UDP datagram from a client
          */
-        //VLOG(DEBUG, "waiting from server \n");
         if (recvfrom(sockfd, buffer, MSS_SIZE, 0,
                      (struct sockaddr *)&clientaddr, (socklen_t *)&clientlen) < 0)
         {
@@ -102,37 +125,24 @@ int main(int argc, char **argv)
         }
         recvpkt = (tcp_packet *)buffer;
         assert(get_data_size(recvpkt) <= DATA_SIZE);
+        // if it is an empty pkt that signifies EOF
         if (recvpkt->hdr.data_size == 0)
         {
-            //VLOG(INFO, "End Of File has been reached");
             continue;
         }
-        /* 
-         * sendto: ACK back to the client 
-         */
+        
         gettimeofday(&tp, NULL);
         VLOG(DEBUG, "%lu, %d, %d", tp.tv_sec, recvpkt->hdr.data_size, recvpkt->hdr.seqno);
 
-        next_seqno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
-
         // if there is no gap in the order of the pkts received
-        if (next_seqno - cur_seqno <= DATA_SIZE)
+        if (recvpkt->hdr.seqno - cur_seqno <= DATA_SIZE)
         { 
-            cur_seqno = next_seqno; // update current sequence number
+            cur_seqno = recvpkt->hdr.seqno; // update current sequence number
 
-            fp = fopen(argv[2], "r+");
-            fseek(fp, recvpkt->hdr.seqno, SEEK_SET);
-            fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp);
-            fclose(fp);
-            printf("pkt with sequence number %d, data_size %d, written to file\n", recvpkt->hdr.seqno, recvpkt->hdr.data_size);
-            sndpkt = make_packet(0);
-            sndpkt->hdr.ackno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
-            sndpkt->hdr.ctr_flags = ACK;
-            if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0,
-                       (struct sockaddr *)&clientaddr, clientlen) < 0)
-            {
-                error("ERROR in sendto");
-            }
+            write_to_file(file_name, fp, recvpkt->hdr.seqno, recvpkt->data, recvpkt->hdr.data_size);
+
+            ackno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
+            send_ACK(ackno);
         }
     }
 
